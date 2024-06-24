0876734598971c5195dd13734768e0cc78cdd45a
unknown <ricardodonado1105@gmail.com>
2024-06-23 20:36:40 -0500

    mobile version scripts/styles finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
 const shareButton = document.querySelectorAll(".shareButton")
 const shareBox = document.getElementById("share-activated")
+const shareBoxMobile = document.getElementById("shareButton")
+const profileElement = document.getElementById("activeMobile")
+const mainArticleElement = document.getElementById("mainArticle")
+
+console.log(shareBoxMobile[0])
+
 shareButton[0].addEventListener("click", () =>{
     shareButton[0].classList.toggle("active")
     shareBox.classList.toggle("unactive")
+    shareBoxMobile.classList.toggle("shareButtonMobileActive")
+    mainArticleElement.classList.toggle("mobileActiveMargin")
+    if (profileElement.id === "activeMobile"){
+        profileElement.id = "unactiveMobile"
+    }else{
+        profileElement.id = "activeMobile"
+    }
 })
